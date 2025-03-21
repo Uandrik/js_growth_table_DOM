08b99abf0b1191c33f2f381efd443ce4925aafcb
Uandrik <uandriy4141@gmail.com>
2025-03-21 06:51:41 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const appendRow = document.querySelector('.append-row');
+const removeRow = document.querySelector('.remove-row');
+const appendColumn = document.querySelector('.append-column');
+const removeColumn = document.querySelector('.remove-column');
+
+const table = document.querySelector('.field');
+const tr = document.querySelector('tr');
+const allTr = table.querySelectorAll('tr');
+const columns = tr.querySelectorAll('td');
+
+appendRow.addEventListener('click', () => {
+  const newTr = document.createElement('tr');
+
+  for (let i = 0; i < columns.length; i++) {
+    const newTd = document.createElement('td');
+
+    newTr.appendChild(newTd);
+  }
+  table.append(newTr);
+});
+
+removeRow.addEventListener('click', () => {
+  table.lastElementChild.remove();
+});
+
+appendColumn.addEventListener('click', () => {
+  allTr.forEach((t) => {
+    const newTd = document.createElement('td');
+
+    t.appendChild(newTd);
+  });
+});
+
+removeColumn.addEventListener('click', () => {
+  allTr.forEach((t) => {
+    t.lastElementChild.remove();
+  });
+});
